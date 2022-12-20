8d64f5c7b321ac087cb0672d01fa432db383e75f
gmanu0077 <66879853+gmanu0077@users.noreply.github.com>
2022-12-21 00:33:36 +0530

    fin

--- server/user.js
+++ server/user.js
@@ -1,79 +1,80 @@
 const router = require('express').Router();
 let User = require('./models/user.mod');
 const bcrypt = require('bcrypt');
-const jwt=require('jsonwebtoken');
-   
+const jwt = require('jsonwebtoken');
+
 
 
 router.route('/').get((req, res) => {
+  console.log("///")
   User.find()
     .then(users => res.json(users))
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
-router.route('/add').post(async(req, res) => {
- 
-  const{username,password,passwordVerify} = req.body;
-  
-   if(!username || !password || !passwordVerify)
-   return res
-    .status(400)
-    .json({ errorMessage: "Please enter all required fields." });
+router.route('/add').post(async (req, res) => {
 
- if (password.length < 6)
-  return res.status(400).json({
-    errorMessage: "Please enter a password of at least 6 characters.",
-  });
+  const { username, password, passwordVerify } = req.body;
 
-if (password !== passwordVerify)
-  return res.status(400).json({
-    errorMessage: "Please enter the same password twice.",
-  });
+  if (!username || !password || !passwordVerify)
+    return res
+      .status(400)
+      .json({ errorMessage: "Please enter all required fields." });
+
+  if (password.length < 6)
+    return res.status(400).json({
+      errorMessage: "Please enter a password of at least 6 characters.",
+    });
+
+  if (password !== passwordVerify)
+    return res.status(400).json({
+      errorMessage: "Please enter the same password twice.",
+    });
 
 
   const exist = await User.findOne({ username })
-  if(exist){
+  if (exist) {
     return res.status(400).json({
       errorMessage: "An account with this username already exists.",
     })
   }
- 
-  
- 
+
+
+
   const salt = await bcrypt.genSalt();
   const passwordHash = await bcrypt.hash(password, salt);
 
-  
+
   const newUser = new User({
     username,
     passwordHash
-    
+
   });
-  
+
 
   const saved = await newUser.save()
-   
-   
 
-    const token = jwt.sign(
-      {
-        user: saved._id,
-        name: saved.username,
-        status:true
 
-      },
-      process.env.JWT_SEC
-       
-    );
-   
-    res
-      .cookie("token", token, {
-        httpOnly: true,
-        secure: true,
-        sameSite: "none",
-      })
-      .send();
-  
+
+  const token = jwt.sign(
+    {
+      user: saved._id,
+      name: saved.username,
+      status: true
+
+    },
+    process.env.JWT_SEC
+
+  );
+
+  res
+    .cookie("token", token, {
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+    })
+    .send();
+
 
 });
 router.route("/login").post(async (req, res) => {
@@ -104,24 +105,24 @@ router.route("/login").post(async (req, res) => {
       {
         user: existingUser._id,
         name: existingUser.username,
-        status:true
+        status: true
       },
       process.env.JWT_SEC
     );
-    
-    
+
+
 
     // send the token in a HTTP-only cookie
 
     res.cookie("token", token, {
-        httpOnly: true,
-        secure: true,
-        sameSite: "none",
-        
-        
-      })
+      httpOnly: true,
+      secure: true,
+      sameSite: "none",
+
+
+    })
       .send();
-      
+
   } catch (err) {
     console.error(err);
     res.status(500).send();
@@ -130,28 +131,28 @@ router.route("/login").post(async (req, res) => {
 
 router.route("/logout").get((req, res) => {
   res.cookie("token", "", {
-      httpOnly: true,
-      expires: new Date(0),
-      secure: true,
-      sameSite: "none",
-    })
+    httpOnly: true,
+    expires: new Date(0),
+    secure: true,
+    sameSite: "none",
+  })
     .send();
 });
 
 router.get("/loggedIn", (req, res) => {
   try {
-    
+
     const token = req.cookies.token;
     if (!token) return res.json(false)
-    
-    
-    
-    const verify=jwt.verify(token, process.env.JWT_SEC);
-    
-    if(verify){
-     
-    res.send(verify)
-   }
+
+
+
+    const verify = jwt.verify(token, process.env.JWT_SEC);
+
+    if (verify) {
+
+      res.send(verify)
+    }
   } catch (err) {
     res.json(false);
   }
