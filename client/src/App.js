5fcfa75ee80c2090bb1221a71936f76a015c5ccc
gmanu0077 <66879853+gmanu0077@users.noreply.github.com>
2022-12-21 00:11:17 +0530

    final edit

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Router from "./Router";
 import axios from "axios";
+import "./App.css"
 import { AuthContextProvider } from "./context/AuthContext";
 
 axios.defaults.withCredentials = true;
--- client/src/Router.js
+++ client/src/Router.js
@@ -1,8 +1,8 @@
 import Old from './old'
 import './App.css';
-import React, {useContext} from 'react';
+import React, { useContext } from 'react';
 import Front from './front'
-import {BrowserRouter, Redirect, Route, Switch} from 'react-router-dom'
+import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom'
 import User from './user'
 import Create from './create'
 import Reg from './register'
@@ -13,48 +13,48 @@ import AuthContext from "./context/AuthContext";
 
 
 function Router() {
-  var {loggedin} = useContext(AuthContext);
+  var { loggedin } = useContext(AuthContext);
 
- 
-  
+
+
   return (
-  
+
     <div>
-    <h3 className="white-text center">BEAT MAKER</h3>
-    
-     <BrowserRouter>
-     <Switch>
-   
-     <Route exact path="/" component={User}/>
-    
-     
-     <Route path="/register" component={Reg}/>
-     
-     
-     <>
-    
-      <h2 className="brown-text text-lighten-3 center"> WELCOME</h2>
-     
-      <h2 className="deep-orange-text center"> {loggedin?.name || "Beat-Master"}</h2>
-      <Route exact path="/beatsmaker" component={Front} />
-        
-      <Route exact  path="/createbeats" component={Create}/>
-      <Route exact path="/mybeats" component={Old}/>
-      
-     
-      
-      </>
-        
-        
-        
-        
-      
-     
-      </Switch>
-     
-     </BrowserRouter>
-     </div>
-    
+      <h3 className="white-text center">BEAT MAKER</h3>
+
+      <BrowserRouter>
+        <Switch>
+
+          <Route exact path="/" component={User} />
+
+
+          <Route path="/register" component={Reg} />
+
+
+          <>
+
+            <h2 className="brown-text text-lighten-3 center"> WELCOME</h2>
+
+            <h2 className="deep-orange-text center"> {loggedin?.name || "Beat-Master"}</h2>
+            <Route exact path="/beatsmaker" component={Front} />
+
+            <Route exact path="/createbeats" component={Create} />
+            <Route exact path="/mybeats" component={Old} />
+
+
+
+          </>
+
+
+
+
+
+
+        </Switch>
+
+      </BrowserRouter>
+    </div>
+
   );
 }
 
